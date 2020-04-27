736e203e03d9af999ced1f7bfbd370644a722707
Linas B <linasb20@gmail.com>
2020-04-27 19:18:15 +0300

    -

---
+++ PSA_Baras/Controllers/CartItemsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PSA_Baras.Data;
+using PSA_Baras.Models;
+
+namespace PSA_Baras.Controllers
+{
+    public class CartItemsController : Controller
+    {
+        private readonly BarasDBContext _context;
+
+        public CartItemsController(BarasDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CartItems
+        public async Task<IActionResult> Index()
+        {
+            var barasDBContext = _context.CartItem.Include(c => c.cart).Include(c => c.cocktail);
+            return View(await barasDBContext.ToListAsync());
+        }
+
+        // GET: CartItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItem
+                .Include(c => c.cart)
+                .Include(c => c.cocktail)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Create
+        public IActionResult Create()
+        {
+            ViewData["cartId"] = new SelectList(_context.Cart, "Id", "Id");
+            ViewData["cocktailId"] = new SelectList(_context.Cocktail, "Id", "Id");
+            return View();
+        }
+
+        // POST: CartItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,count,price,cartId,cocktailId")] CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cartItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["cartId"] = new SelectList(_context.Cart, "Id", "Id", cartItem.cartId);
+            ViewData["cocktailId"] = new SelectList(_context.Cocktail, "Id", "Id", cartItem.cocktailId);
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItem.FindAsync(id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["cartId"] = new SelectList(_context.Cart, "Id", "Id", cartItem.cartId);
+            ViewData["cocktailId"] = new SelectList(_context.Cocktail, "Id", "Id", cartItem.cocktailId);
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,count,price,cartId,cocktailId")] CartItem cartItem)
+        {
+            if (id != cartItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cartItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartItemExists(cartItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["cartId"] = new SelectList(_context.Cart, "Id", "Id", cartItem.cartId);
+            ViewData["cocktailId"] = new SelectList(_context.Cocktail, "Id", "Id", cartItem.cocktailId);
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItem
+                .Include(c => c.cart)
+                .Include(c => c.cocktail)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cartItem = await _context.CartItem.FindAsync(id);
+            _context.CartItem.Remove(cartItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartItemExists(int id)
+        {
+            return _context.CartItem.Any(e => e.Id == id);
+        }
+    }
+}
